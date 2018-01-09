50237ce7e4fbbbf557f04a6eb91f6f44357911d7
guimingming <ahqsgmm@163.com>
2018-01-09 10:39:47 +0800

    代码格式调整

--- src/main/java/com/study/kafka/election/LeaderElection.java
+++ src/main/java/com/study/kafka/election/LeaderElection.java
@@ -18,14 +18,14 @@ import org.apache.zookeeper.ZooKeeper;
 import org.apache.zookeeper.data.Stat;
 
 public class LeaderElection {
-	
+
 	private static Object lock = new Object();
 
 	private final String ROOT = "/kafka";
 
 	private final String LEADER = "/kafka/leader";
-	
-	private final byte[] DEFAULT_DATA = {0x11, 0x12};
+
+	private final byte[] DEFAULT_DATA = { 0x11, 0x12 };
 
 	private ZooKeeper zk;
 
@@ -44,14 +44,14 @@ public class LeaderElection {
 	private String createLeaderNode(String node) throws Exception {
 		return zk.create(LEADER, localhost, Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
 	}
-	
+
 	private void checkRootExists(String root) throws Exception {
 		Stat state = zk.exists(ROOT, false);
 		if (state == null) {
 			zk.create(ROOT, DEFAULT_DATA, Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
 		}
 	}
-	
+
 	public void electorUntilGetLeaderShip() throws Exception {
 		while (true) {
 			synchronized (lock) {
@@ -59,51 +59,51 @@ public class LeaderElection {
 				String first = sortedSetNodes.first();// 获取最小节点
 				if ((znode).equals(first)) {
 					// 当前节点为leader
-					byte[] data = zk.getData(first, null, null);  
+					byte[] data = zk.getData(first, null, null);
 					System.out.println("当前节点为leader: " + znode + " data: " + new String(data, "UTF-8"));
 					lock.notify();
-					return ;
-				} 
+					return;
+				}
 				// 监听前一个节点
-		        String preNode = sortedSetNodes.floor(znode);
-		        zk.getData(preNode, new Watcher() {
+				String preNode = sortedSetNodes.floor(znode);
+				zk.getData(preNode, new Watcher() {
 					public void process(WatchedEvent event) {
 						if (event.getType() == EventType.NodeDeleted) {
 							try {
 								TreeSet<String> sortedSetNodes = getChildNodes();
-                                     // 获取最小节点
+								// 获取最小节点
 								String first = sortedSetNodes.first();
 								if ((znode).equals(first)) {
 									// 当前节点为leader
-									byte[] data = zk.getData(first, null, null);  
+									byte[] data = zk.getData(first, null, null);
 									System.out.println("当前节点为leader: " + znode + " data: " + new String(data, "UTF-8"));
-					                lock.notify();  
+									lock.notify();
 									return;
-								} 
+								}
 							} catch (Exception e) {
 								e.printStackTrace();
 							}
 						}
 					}
 				}, null);
-		        lock.wait(); 
+				lock.wait();
 			}
 		}
 	}
-	
+
 	public boolean elector() throws Exception {
 		TreeSet<String> sortedSetNodes = getChildNodes();
 		// 获取最小节点
 		String first = sortedSetNodes.first();
 		if ((znode).equals(first)) {
 			// 当前节点为leader
-			byte[] data = zk.getData(first, null, null);  
+			byte[] data = zk.getData(first, null, null);
 			System.out.println("当前节点为leader: " + znode + " data: " + new String(data, "UTF-8"));
 			return true;
-		} 
+		}
 		// 监听前一个节点
-        String preNode = sortedSetNodes.floor(znode);
-        zk.getData(preNode, new Watcher() {
+		String preNode = sortedSetNodes.floor(znode);
+		zk.getData(preNode, new Watcher() {
 			public void process(WatchedEvent event) {
 				if (event.getType() == EventType.NodeDeleted) {
 					try {
@@ -111,10 +111,10 @@ public class LeaderElection {
 						String first = sortedSetNodes.first();// 获取最小节点
 						if ((znode).equals(first)) {
 							// 当前节点为leader
-							byte[] data = zk.getData(first, null, null);  
+							byte[] data = zk.getData(first, null, null);
 							System.out.println("当前节点为leader: " + znode + " data: " + new String(data, "UTF-8"));
 							return;
-						} 
+						}
 					} catch (Exception e) {
 						e.printStackTrace();
 					}
@@ -123,7 +123,6 @@ public class LeaderElection {
 		}, null);
 		return false;
 	}
-
 
 	private TreeSet<String> getChildNodes() throws KeeperException, InterruptedException {
 		List<String> nodes = zk.getChildren(ROOT, null);
@@ -134,31 +133,31 @@ public class LeaderElection {
 		return sortedSetNodes;
 	}
 
-	public static String getLocalIpAddress() throws SocketException {  
-        // 获得本机的所有网络接口  
-        Enumeration<NetworkInterface> nifs = NetworkInterface.getNetworkInterfaces();  
-  
-        while (nifs.hasMoreElements()) {  
-            NetworkInterface nif = nifs.nextElement();  
-  
-            // 获得与该网络接口绑定的 IP 地址，一般只有一个  
-            Enumeration<InetAddress> addresses = nif.getInetAddresses();  
-            while (addresses.hasMoreElements()) {  
-                InetAddress addr = addresses.nextElement();  
-  
-                String ip = addr.getHostAddress();  
-                // 只关心 IPv4 地址  
-                if (addr instanceof Inet4Address && !"127.0.0.1".equals(ip)) {  
-                    return ip;  
-                }  
-            }  
-        }  
-        return null;  
-    }  
-  
-    public static byte[] getLocalIpAdressBytes() throws SocketException {  
-        String ip = getLocalIpAddress();  
-        return ip == null ? null : ip.getBytes();  
-    }  
+	public static String getLocalIpAddress() throws SocketException {
+		// 获得本机的所有网络接口
+		Enumeration<NetworkInterface> nifs = NetworkInterface.getNetworkInterfaces();
+
+		while (nifs.hasMoreElements()) {
+			NetworkInterface nif = nifs.nextElement();
+
+			// 获得与该网络接口绑定的 IP 地址，一般只有一个
+			Enumeration<InetAddress> addresses = nif.getInetAddresses();
+			while (addresses.hasMoreElements()) {
+				InetAddress addr = addresses.nextElement();
+
+				String ip = addr.getHostAddress();
+				// 只关心 IPv4 地址
+				if (addr instanceof Inet4Address && !"127.0.0.1".equals(ip)) {
+					return ip;
+				}
+			}
+		}
+		return null;
+	}
+
+	public static byte[] getLocalIpAdressBytes() throws SocketException {
+		String ip = getLocalIpAddress();
+		return ip == null ? null : ip.getBytes();
+	}
 
 }
